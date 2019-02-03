180ec02c03a3ed940c85aaf6574d0e7c7de23b87
JohnPAfr <john.aurry@gmail.com>
2019-02-03 11:37:14 +0100

    First try with SASS

--- src/component/CssPractice.js
+++ src/component/CssPractice.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react';
+import './style.sass'
 
 class CssPractice extends Component {
 
-    hash (s) {
-        var h = 7
-        var letters = "acdegilmnoprstuw"
-        
-        for(var i = 0; i < s.length; i++) {
-            h = (h * 37 + letters.indexOf(s[i]))
-        }
-        return h
-    }
-
     render() {
         return (
-            <div className='App'>
-                <h1>Result :</h1>
-                <table>
-                    <tr>a : {this.hash('a')}</tr>
-                    <tr>w : {this.hash('w')}</tr>
-
-                    <tr>aa : {this.hash('aa')}</tr>
-                    <tr>aw : {this.hash('aw')}</tr>
-
-                    <tr>agile : {this.hash('agile')}</tr>
-                    <tr>agil : {this.hash('agil')}</tr>
-
-                    
-
-                </table>
+            <div>
+               <div className='container'>
+                   <h1 className='header'>CSS GRID PRACTICE</h1>
+               </div>
             </div>
         )
     }
